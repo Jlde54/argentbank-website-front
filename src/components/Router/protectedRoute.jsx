4893bcd6da1fc45c04548d4a59f2298ab16ef41c
Jlde54 <jlde54@sfr.fr>
2023-12-25 13:38:11 +0100

    add protected routes

--- src/components/Router/protectedRoute.jsx
+++ src/components/Router/protectedRoute.jsx
@@ -1,6 +1,5 @@
 import { useSelector } from 'react-redux'
 import { Navigate, Outlet } from 'react-router-dom'
-// Outlet is used to serve as a parent for multiple child routes.
 
 const ProtectedRoute = () => {
   const token  = useSelector((state) => state.user.token)
